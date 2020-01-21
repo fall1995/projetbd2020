4b51f2089bae4b3869d27caf24fddf52c94eb628
fall1995 <alyabderrahmanefall@gmail.com>
2020-01-21 17:33:58 +0100

    Festivale DAO et sevellete

---
+++ client/bd-project-2020/serveur/src/main/java/mesServelets/FestivalServelet.java
@@ -0,0 +1,54 @@
+package mesServelets;
+
+import java.io.IOException;
+import java.sql.Date;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.HashMap;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import DAO.FestivalDAO;
+import mesClasses.Festival;
+
+public class FestivalServelet extends HttpServlet{
+	@Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) 
+            throws ServletException, IOException {
+        
+        System.out.println( "========================================================== FestivalServlet [doGet] =========================================================" );
+        response.setContentType("application/json");
+        response.addHeader("Access-Control-Allow-Origin", "*");
+        Enumeration<String> P = request.getParameterNames();
+        HashMap<String, String> parametres = new HashMap();
+       
+
+        while (P.hasMoreElements()) {
+            String p = P.nextElement();
+            parametres.put(p, request.getParameter((String) p));
+        }
+       
+
+        String  domaine, ville, datedebut, datefin ;
+        domaine = parametres.get("domaine"); 
+        ville = parametres.get("ville");
+        datedebut = parametres.get("dateDebut");
+        datefin = parametres.get("dateDebut");
+        /*
+         * Dans cette partie faudra transformer datedebut et datefin en Date
+         * 
+         */
+        ArrayList<Festival> resultat = new ArrayList<Festival>();
+        FestivalDAO festDao = new FestivalDAO();
+        // la methode suivante ne va pas marcher a cause du type Date 
+        // 
+        //resultat = festDao.getFestival(domaine, datedebut, datefin, ville);
+
+
+    }
+}
+
