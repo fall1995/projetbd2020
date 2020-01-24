6be2f2b90526d89f73e8650e2afaf869aac0f324
ilyes38400 <ilyes38400@gmail.com>
2020-01-24 14:16:12 +0100

    client

--- client/bd-project-2020/serveur/src/main/java/DAO/FestivalDAO.java
+++ client/bd-project-2020/serveur/src/main/java/DAO/FestivalDAO.java
@@ -137,6 +137,118 @@ public class FestivalDAO extends SQLAble implements FestivalInterface {
 		return festivals;
 		// return null;
 	}
+	
+	public ArrayList<Festival> filtreDomaineSql(String domainesql) {
+
+		// connection à la base
+		try {
+			connectToDatabase();
+		} catch (ClassNotFoundException | SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		String s = "null";
+		System.out.println("domaine sql =" +domainesql);
+		domainesql = "'"+domainesql+"'";
+		ArrayList<Festival> festivals = new ArrayList<Festival>();// Liste pour recuperer mon resultat
+
+		// requete si l'utilisateur n'a pas affiné sa recherche
+		System.out.println("avant try \n");
+		try {
+			Festival fest;
+			// PreparedStatement ps;
+			Statement ps = conn.createStatement();
+			System.out.println("avant select \n");
+			// ps = conn.prepareStatement("SELECT * FROM LesFestivals");
+			String query = "SELECT * FROM LesFestivals where domaine="+domainesql+"";
+			// ps.setInt(1, 0);
+			System.out.println(" apres ps \n");
+			ResultSet resultats = ps.executeQuery(query);
+
+			// Parcours des resulats (objet ResulSet) retournés par executeQuery()
+			String idFestival;
+
+			String nomFestival;
+
+			String domaine;
+			String complementDomaine;
+			String region;
+			int departement;
+			String periodicite;
+			String moiHabDebut;
+			String siteWeb;
+			String commune;
+			Date dateDebut;
+			Date dateFin;
+			Date dateCreation;
+			int codepost;
+			String codeINSEE;
+			float coord1;
+			float coord2;
+			String nomDepartement;
+			int nbPlaceLouees;
+			Date dateAjout;
+			int idUtilisateur;
+
+			while (resultats.next()) {
+				idFestival = resultats.getString(1);
+				// System.out.println("id festival = "+idFestival + "\n");
+				nomFestival = resultats.getString(2);
+				// System.out.println("nomFestival = "+nomFestival + "\n");
+				domaine = resultats.getString(3);
+				// System.out.println("domaine = "+domaine + "\n");
+				complementDomaine = resultats.getString(4);
+				// System.out.println("complement dom = "+complementDomaine + "\n");
+				region = resultats.getString(5);
+				departement = resultats.getInt(6);
+				periodicite = resultats.getString(7);
+				moiHabDebut = resultats.getString(8);
+				// System.out.println("moiHabDebut = "+moiHabDebut + "\n");
+				siteWeb = resultats.getString(9);
+				// System.out.println("siteWeb = "+siteWeb + "\n");
+				commune = resultats.getString(10);
+				// System.out.println("commune = "+commune + "\n");
+				dateDebut = resultats.getDate(11);
+				// System.out.println("dateDebut = "+dateDebut + "\n");
+				dateFin = resultats.getDate(12);
+				// System.out.println("dateFin = "+dateFin + "\n");
+				dateCreation = resultats.getDate(13);
+				// System.out.println("datecreation = "+dateCreation + "\n");
+				codepost = resultats.getInt(14);
+				// System.out.println("codeP = "+codepost + "\n");
+				codeINSEE = resultats.getString(15);
+				// System.out.println("codeInsee = "+codeINSEE + "\n");
+				coord1 = resultats.getFloat(16);
+				// System.out.println("coord1 = "+coord1 + "\n");
+				coord2 = resultats.getFloat(17);
+				// System.out.println("coord2 = "+coord2 + "\n");
+				nomDepartement = resultats.getString(18);
+				// System.out.println("nomDepartement = "+nomDepartement + "\n");
+				nbPlaceLouees = resultats.getInt(19);
+				dateAjout = resultats.getDate(20);
+				idUtilisateur = resultats.getInt(21);
+				// System.out.println("id utilisateur "+idUtilisateur);
+
+				// j'instancie la classe festival et je l'ajoute a ma liste de festivale
+				fest = new Festival(idFestival, nomFestival, domaine, complementDomaine, region, departement,
+						periodicite, moiHabDebut, siteWeb, commune, dateDebut, dateFin, dateCreation, codepost,
+						codeINSEE, coord1, coord2, nomDepartement, nbPlaceLouees, dateAjout, idUtilisateur);
+				// System.out.println("Element de ma liste "+ fest.getIdUtilisateur());
+				festivals.add(fest);
+			}
+			resultats.close();
+
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+		}
+
+		System.out.println("\n" + festivals.size());
+		return festivals;
+		// return null;
+	}
+	
+	
 	public ArrayList<Festival> filtreVilleSQL(String ville) {
 
 		// connection à la base
@@ -256,17 +368,12 @@ public class FestivalDAO extends SQLAble implements FestivalInterface {
 		System.out.println("Ville = " + Ville + "\n");
 
 		if (aucunFiltre(domaineF, dateDebutF, dateFinF, Ville)) {
-			System.out.println("dans aucun filtre");
+			//System.out.println("dans aucun filtre");
 			festivals = this.getFestival();
 			 return festivals;
 		}
-		System.out.println("apres aucun filtre");
+	//	System.out.println("apres aucun filtre");
 		// filtre uniquement sur ville
-		if (villeFiltre(domaineF, dateDebutF, dateFinF, Ville)) {
-			ArrayList<Festival> resultat = new ArrayList<Festival>();
-			
-			
-		}
 		
 		if (villeFiltre(domaineF, dateDebutF, dateFinF, Ville)) {
 			festivals = this.filtreVilleSQL(Ville);
@@ -274,7 +381,12 @@ public class FestivalDAO extends SQLAble implements FestivalInterface {
 		}	
 		// filtre uniquement sur domaine
 		if (domaineFiltre(domaineF, dateDebutF, dateFinF, Ville)) {
-			System.out.println("dans la fonction");
+			System.out.println("bone voie");
+			festivals = this.filtreDomaineSql(domaineF);
+			return festivals;
+			
+			
+			/*System.out.println("dans la fonction");
 			ArrayList<Festival> resultat = new ArrayList<Festival>();
 			for (int i = 0; i < festivals.size(); i++) {
 				// test domaine
@@ -283,7 +395,7 @@ public class FestivalDAO extends SQLAble implements FestivalInterface {
 					resultat.add(festivals.get(i));
 				}
 			}
-			return resultat;
+			return resultat;*/
 		}
 		// filtre uniquement sur date
 		if (dateFiltre(domaineF, dateDebutF, dateFinF, Ville)) {
@@ -405,7 +517,7 @@ public class FestivalDAO extends SQLAble implements FestivalInterface {
 	}
 
 	boolean domaineFiltre(String domaineF, String dateDebutF, String dateFinF, String Ville) {
-		System.out.println("dans boolean");
+		//System.out.println("dans boolean");
 		return ((domaineF != null || !domaineF.isEmpty()) && (dateDebutF == null || dateDebutF.isEmpty())
 				&& (dateFinF == null || dateFinF.isEmpty()) && (Ville == null || Ville.isEmpty()));
 	}
