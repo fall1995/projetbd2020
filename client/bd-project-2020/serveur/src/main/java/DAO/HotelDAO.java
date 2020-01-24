de67629ac250314c15bbdc737017a6b33c719745
BOUAZIZ Lamine <lam.bouaziz96@gmail.com>
2020-01-24 11:54:34 +0100

    commit_DAOHotel

---
+++ client/bd-project-2020/serveur/src/main/java/DAO/HotelDAO.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DAO;
+
+import ConnexionBase.SQLAble;
+import DAOInterfaces.HebergementInterface;
+import java.sql.Date;
+import java.sql.SQLException;
+import mesClasses.Hebergement;
+
+/**
+ *
+ * @author bouaziz
+ */
+public class HotelDAO extends SQLAble implements HebergementInterface{
+
+    @Override
+    public void add(Hebergement h) throws SQLException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void update(Hebergement h) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void getHebergementsProches(int iDFestival, int nbPlaces, Date dateDebut, Date dateFin, String typeHebergement) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void getOneHebergement(Hebergement h) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
